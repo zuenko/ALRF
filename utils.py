a18c33b8c871b92487110defcd26287c6a595492
Denis Zuenko <neron.v2@gmail.com>
2018-12-19 19:56:59 +0300

    Me2uMoM learning

--- utils.py
+++ utils.py
@@ -1,19 +1,37 @@
 import torch.utils.data
 from torchvision import datasets, transforms
+import torch.utils.data as data
 
-def load_mnist(batch_size = 128, batch_size_test = 1000):
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data/mnist_data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data/mnist_data', train=False, transform=transforms.Compose([
+def build_dataset(dataset='MNIST', dataset_dir='./data', batch_size=100):
+    dataset_ = {
+        'MNIST': datasets.MNIST,
+        'CIFAR10': datasets.CIFAR10
+    }[dataset]
+    
+    transform = {
+        'MNIST': transforms.ToTensor(),
+        'CIFAR10': transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
-        batch_size=batch_size_test, shuffle=True)
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+    }[dataset]
+    
+    train_dataset = dataset_(root=dataset_dir,
+                             train=True,
+                             transform=transform,
+                             download=True)
+
+    train_loader = data.DataLoader(dataset=train_dataset,
+                                  batch_size=batch_size,
+                                  shuffle=True)
+
+    test_dataset = dataset_(root=dataset_dir,
+                             train=False,
+                             transform=transform,
+                             download=True)
+
+    test_loader = data.DataLoader(dataset=test_dataset,
+                                  batch_size=batch_size,
+                                  shuffle=False)
     
     return train_loader, test_loader
