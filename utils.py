7c65c5285755c45b1c0b137038037ef91cd80d1c
jurg96 <jurg96@gmail.com>
2018-12-19 14:55:04 +0300

    changes

--- utils.py
+++ utils.py
@@ -3,14 +3,14 @@ from torchvision import datasets, transforms
 
 def load_mnist(batch_size = 128, batch_size_test = 1000):
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=True, download=True,
+        datasets.MNIST('./data/mnist_data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=False, transform=transforms.Compose([
+        datasets.MNIST('./data/mnist_data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
