f587dd589d7f69affe6663b7032048c5816c6266
jurg96 <jurg96@gmail.com>
2018-12-20 02:27:09 +0300

    add STL10

--- nets/__init__.py
+++
@@ -1,3 +0,0 @@
-from .NetOneLayerLowRank import *
-from .NetOneLayer import *
-from .NetMLPLowRank import *
--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ import torch.utils.data as data
 def build_dataset(dataset='MNIST', dataset_dir='./data', batch_size=100):
     dataset_ = {
         'MNIST': datasets.MNIST,
-        'CIFAR10': datasets.CIFAR10
+        'CIFAR10': datasets.CIFAR10,
+        'STL10': datasets.STL10
     }[dataset]
     
     transform = {
@@ -13,7 +14,11 @@ def build_dataset(dataset='MNIST', dataset_dir='./data', batch_size=100):
         'CIFAR10': transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-            ])
+        ]),
+        'STL10':transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]),
     }[dataset]
     
     train_dataset = dataset_(root=dataset_dir,
