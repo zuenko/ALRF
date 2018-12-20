e6e3fa526133df57feea3e724e91ffa942ff7134
Denis Zuenko <neron.v2@gmail.com>
2018-12-20 03:22:06 +0300

    fixiki fixyat my lass

--- utils.py
+++ utils.py
@@ -6,12 +6,17 @@ import torch.utils.data as data
 def build_dataset(dataset='MNIST', dataset_dir='./data', batch_size=100):
     dataset_ = {
         'MNIST': datasets.MNIST,
-        'CIFAR10': datasets.CIFAR10
+        'CIFAR10': datasets.CIFAR10,
+        'STL10': datasets.STL10
     }[dataset]
     
     transform = {
         'MNIST': transforms.ToTensor(),
         'CIFAR10': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ]),
+        'STL10': transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
             ])
